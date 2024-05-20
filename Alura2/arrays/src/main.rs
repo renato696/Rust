d4635e1c6ccab488cdf423ee06b3e678a0835f05
renato696 <renato696@gmail.com>
2024-05-20 11:09:03 -0300

    arrays type

--- Alura2/arrays/src/main.rs
+++ Alura2/arrays/src/main.rs
@@ -1,8 +1,6 @@
 fn main() {
-    let nota1: f32 = 10f32; //uso o f32 depois do 10 para forçar o float quando seria um integer
-    let nota2: f32 = 8f32;
-    let nota3: f32 = 9.5;
-    let nota4: f32 = 6.0; // adicionando o ponto resolve o f32  
+    
+    let notas: [f32; 4] = [10f32, 8f32, 9.5, 6.0]; //array
 
-    println!(" Nota 1 = {}, Nota 2 = {} , Nota 3 = {} e Nota 4 = {}", nota1, nota2, nota3, nota4);
+    println!(" Nota 1 = {}, Nota 2 = {} , Nota 3 = {} e Nota 4 = {}", notas[0], notas[1], notas[2], notas[3]);
 }    
