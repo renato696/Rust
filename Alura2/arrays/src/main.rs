326ade81878cfeac63a36c09c5821245a2157ab7
renato696 <renato696@gmail.com>
2024-05-20 11:38:13 -0300

    array multidimensional

--- Alura2/arrays/src/main.rs
+++ Alura2/arrays/src/main.rs
@@ -1,8 +1,24 @@
 fn main() {
     
-    let notas: [f32; 4] = [10f32, 8f32, 9.5, 6.0]; //array
+    let notas: [f32; 4] = [6.5; 4]; //array
 
     for indice in 0..notas.len() {
         println!("A nota {} é = {}", indice + 1, notas[indice]);
     }
+
+    matriz();
 }    
+
+fn matriz(){
+    let matriz: [[f32; 3]; 2]= [ //possibilidade de tipar a matriz - tipo, itens, linhas
+        [0.0, 1.2, 0.1],
+        [1.3, 0.3, 1.4]
+    ];
+
+    for linha in matriz {
+        for item in linha {
+            println!("Item = {}", item);
+        }
+    }
+
+}
