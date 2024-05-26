1f4a4e5236ce4393027c9a48e4462126d4b742b6
renato696 <renato696@gmail.com>
2024-05-26 13:39:02 -0300

    struct dentro da struct

--- Alura2/structs/src/main.rs
+++ Alura2/structs/src/main.rs
@@ -3,18 +3,24 @@ fn main() {
 }
 
 struct Conta {
-    titular : String,
+    titular : Titular,
     saldo: f64
 }
 
+struct Titular {
+    nome: String,
+    sobrenome: String,
+}
+
 fn conta_corrente(){
-    let conta: Conta = Conta{
-        titular: String::from("Renato Pires"),
+    let titular = Titular{
+        nome: String::from("Renato"), 
+        sobrenome: String::from("Pires")};
+ 
+    let conta: Conta = Conta {
+        titular, //quando a variavel tiver o mesmo valor do atributo, eu posso omitir o valor e deixar apenas o atributo.
         saldo: 100.0
-    }; // essa struct substitui as variáveis abaixo
-
-    //let titular_conta: String = String::from("Renato Pires");
-    //let saldo_conta: f64 = 100.0;
-
-    println!("Dados da Conta: Titular = {}, Saldo = {}", conta.titular, conta.saldo); // como acessar os dados
+    }; 
+    
+    println!("Dados da Conta: Titular = {} {}, Saldo = {}", conta.titular.nome, conta.titular.sobrenome, conta.saldo); // como acessar os dados
 }
