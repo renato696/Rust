ec1ba94e0693aa28c774a243e1583e0292da347b
renato696 <renato696@gmail.com>
2024-05-28 11:25:48 -0300

    rust - update

---
+++ Alura2/structs/target/debug/deps/structs-3c7750c8e02d0fac.d
@@ -0,0 +1,5 @@
+/home/renato/Documents/GitHub/Rust/Alura2/structs/target/debug/deps/libstructs-3c7750c8e02d0fac.rmeta: src/main.rs
+
+/home/renato/Documents/GitHub/Rust/Alura2/structs/target/debug/deps/structs-3c7750c8e02d0fac.d: src/main.rs
+
+src/main.rs:
---
+++ Alura2/structs/target/debug/deps/structs-d46d332a9322a68f.d
@@ -0,0 +1,5 @@
+/home/renato/Documents/GitHub/Rust/Alura2/structs/target/debug/deps/libstructs-d46d332a9322a68f.rmeta: src/main.rs
+
+/home/renato/Documents/GitHub/Rust/Alura2/structs/target/debug/deps/structs-d46d332a9322a68f.d: src/main.rs
+
+src/main.rs:
