fca322134cae2197484527ef4d4a9ec8a6284ad1
email <renato696@gmail.com>
2024-06-25 12:22:54 -0300

    capitulo 1 - final

---
+++ Rust_Programming_Language_Book/hello_cargo/target/debug/deps/hello_cargo-deaad4c17ac37e0b.d
@@ -0,0 +1,5 @@
+/home/renato/Documents/GitHub/Rust/Rust_Programming_Language_Book/hello_cargo/target/debug/deps/hello_cargo-deaad4c17ac37e0b: src/main.rs
+
+/home/renato/Documents/GitHub/Rust/Rust_Programming_Language_Book/hello_cargo/target/debug/deps/hello_cargo-deaad4c17ac37e0b.d: src/main.rs
+
+src/main.rs:
---
+++ Rust_Programming_Language_Book/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/renato/Documents/GitHub/Rust/Rust_Programming_Language_Book/hello_cargo/target/debug/hello_cargo: /home/renato/Documents/GitHub/Rust/Rust_Programming_Language_Book/hello_cargo/src/main.rs
