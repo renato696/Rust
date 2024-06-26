25daed9f32fcc6ff7be2abb3811c4ca981c8c9da
email <renato696@gmail.com>
2024-06-26 11:04:15 -0300

    Guessing Game - 1st part

---
+++ Rust_Programming_Language_Book/guessing_game/target/debug/deps/guessing_game-8b524aa291ee096e.d
@@ -0,0 +1,5 @@
+/home/renato/Documents/GitHub/Rust/Rust_Programming_Language_Book/guessing_game/target/debug/deps/guessing_game-8b524aa291ee096e: src/main.rs
+
+/home/renato/Documents/GitHub/Rust/Rust_Programming_Language_Book/guessing_game/target/debug/deps/guessing_game-8b524aa291ee096e.d: src/main.rs
+
+src/main.rs:
---
+++ Rust_Programming_Language_Book/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/renato/Documents/GitHub/Rust/Rust_Programming_Language_Book/guessing_game/target/debug/guessing_game: /home/renato/Documents/GitHub/Rust/Rust_Programming_Language_Book/guessing_game/src/main.rs
