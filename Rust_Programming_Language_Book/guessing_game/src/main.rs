4685cdbd5b549e557cf0c952e8760846961559b4
email <renato696@gmail.com>
2024-06-28 13:25:02 -0300

    break loop

--- Rust_Programming_Language_Book/guessing_game/src/main.rs
+++ Rust_Programming_Language_Book/guessing_game/src/main.rs
@@ -10,7 +10,6 @@ fn main() {
     println!("The secret number is: {secret_number}");
 
     loop {
-
         println!("Please input your guess.");
 
         let mut guess = String::new();
@@ -29,7 +28,9 @@ fn main() {
         match guess.cmp(&secret_number){
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
         }
     }
 }
