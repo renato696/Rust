30ddcd2978c935458f20c15afd97c1a64f2ea801
email <renato696@gmail.com>
2024-06-28 13:34:56 -0300

    end game

--- Rust_Programming_Language_Book/guessing_game/src/main.rs
+++ Rust_Programming_Language_Book/guessing_game/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    //println!("The secret number is: {secret_number}");
 
     loop {
         println!("Please input your guess.");
@@ -31,6 +31,7 @@ fn main() {
             Ordering::Equal => {
                 println!("You win!");
                 break;
+            }
         }
     }
 }
